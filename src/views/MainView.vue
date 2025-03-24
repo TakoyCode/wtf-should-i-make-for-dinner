d4b7a4e7d5495e840f33c82715a4d11fc0025a7a
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2025-03-24 15:31:09 +0100

    Setup

---
+++ src/views/MainView.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      foodName: 'Taco',
+      randomFoodDish: {},
+    }
+  },
+  methods: {},
+  async mounted() {
+    console.log(this.foodName)
+    try {
+      const response = await axios.get(
+        // 'http://www.matprat.no//api/WeeklyMenuPlanner/GetRandomRecipe',
+        'https://pirate.monkeyness.com/api/insult',
+      )
+      console.log(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+}
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">Make Some</h1>
+  <h1 class="text-3xl font-bold">{{ foodName }}</h1>
+  <button class="">Get Random Dinner</button>
+</template>
