ea86e27f84f031461f767df5471513e186eb81ba
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2025-03-28 15:42:16 +0100

    added primeIcons

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -20,6 +20,19 @@ export default {
         this.isLoading = false
       }
     },
+
+    getDifficultyIcon(difficulty) {
+      switch (difficulty) {
+        case 'enkel':
+          return ''
+        case 'middels':
+          return ''
+        case 'vanskelig':
+          return ''
+        default:
+          return ''
+      }
+    },
   },
   async mounted() {
     this.getRandomDinner()
@@ -31,17 +44,41 @@ export default {
   <div class="container mx-auto" v-if="!isLoading">
     <div class="flex flex-col items-center">
       <h1 class="text-4xl font-bold my-5">Make Some</h1>
-      <img
-        class="rounded h-75"
-        :src="`${randomFoodDish.imageUrl}-jumbotron/large`"
-        :alt="randomFoodDish.imageAlt"
-      />
-      <a
-        :href="`https://www.matprat.no${randomFoodDish.linkUrl}`"
-        target="_blank"
-        class="text-4xl font-bold mt-4 p-4 rounded border-4 border-gray-800 hover:bg-gray-200 active:bg-gray-300 focus:bg-gray-300"
-        >{{ randomFoodDish.name }}</a
-      >
+      <a :href="`https://www.matprat.no${randomFoodDish.linkUrl}`" target="_blank">
+        <img
+          class="rounded h-80"
+          :src="`${randomFoodDish.imageUrl}-jumbotron/large`"
+          :alt="randomFoodDish.imageAlt"
+        />
+      </a>
+      <div class="mt-4 p-4 rounded border-4 border-gray-800">
+        <h1 class="text-4xl text-center w-100 font-bold">
+          {{ randomFoodDish.name }}
+        </h1>
+        <div>
+          <i :class="`pi pi-gauge ${getDifficultyIcon}`"></i>
+          <span class="mx-2">{{ randomFoodDish.difficulty }}</span>
+        </div>
+        <div>
+          <i class="pi pi-user"></i>
+          <span class="mx-1">{{ randomFoodDish.portions }}</span>
+          <!-- <div>
+            <i v-for="i in randomFoodDish.portions" class="pi pi-user mx-1"></i>
+          </div> -->
+        </div>
+        <div>
+          <i class="pi pi-clock"></i>
+          <span class="mx-2">{{ randomFoodDish.preparationTime }}</span>
+        </div>
+        <div>
+          <i v-for="i in randomFoodDish.rating" class="pi pi-star"></i>
+          <span class="mx-1"> {{ randomFoodDish.ratingPrecise }}</span>
+        </div>
+        <div>
+          <i class="pi pi-arrow-down mx-1"></i>
+          <span>Ingrendienser</span>
+        </div>
+      </div>
       <button
         @click="getRandomDinner"
         class="text-white bg-gray-800 hover:bg-gray-700 hover:cursor-pointer font-medium rounded-lg mt-4 px-5 py-2.5"
