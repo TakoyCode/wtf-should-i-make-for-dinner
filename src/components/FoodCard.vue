7e7fc7865202cb8a5314ee0bc4e69542e06665fb
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2025-03-31 14:54:49 +0200

    Made FoodCard Component

---
+++ src/components/FoodCard.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  props: {
+    foodDish: Object,
+  },
+  methods: {
+    getDifficultyIcon(difficulty) {
+      switch (difficulty) {
+        case 'enkel':
+          return ''
+        case 'middels':
+          return ''
+        case 'vanskelig':
+          return ''
+        default:
+          return ''
+      }
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="bg-white">
+    <a :href="`https://www.matprat.no${foodDish.linkUrl}`" target="_blank">
+      <img
+        class="rounded-t h-65"
+        :src="`${foodDish.imageUrl}-jumbotron/large`"
+        :alt="foodDish.imageAlt"
+      />
+    </a>
+    <div class="flex-col justify-items-center p-4 rounded-b border-4 border-gray-800">
+      <h1 class="text-3xl text-center w-100 font-bold">
+        {{ foodDish.name }}
+      </h1>
+
+      <div class="my-1">
+        <i v-for="i in foodDish.rating" class="pi pi-star"></i>
+        <span class="mx-1"> {{ foodDish.ratingPrecise }}</span>
+      </div>
+
+      <div class="flex">
+        <div>
+          <i :class="`pi pi-gauge ${getDifficultyIcon}`"></i>
+          <span class="mx-2">{{ foodDish.difficulty }}</span>
+        </div>
+
+        <div>
+          <i class="pi pi-clock"></i>
+          <span class="mx-2">{{ foodDish.preparationTime }}</span>
+        </div>
+      </div>
+
+      <div class="flex">
+        <div>
+          <i class="pi pi-shopping-cart"></i>
+          <span class="mx-1"> {{ foodDish.ingredients.length }} ingredienser</span>
+        </div>
+
+        <div>
+          <i class="pi pi-user"></i>
+          <span class="mx-1">{{ foodDish.portions }} posjoner</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios'
+import FoodCard from '@/components/FoodCard.vue'
 
 export default {
   data() {
@@ -8,29 +9,18 @@ export default {
       isLoading: true,
     }
   },
+  components: {
+    FoodCard,
+  },
   methods: {
     async getRandomDinner() {
       try {
         const response = await axios.get('/api/recipe')
         this.randomFoodDish = response.data
-        console.log(response.data)
       } catch (error) {
         console.error(error)
       } finally {
         this.isLoading = false
-      }
-    },
-
-    getDifficultyIcon(difficulty) {
-      switch (difficulty) {
-        case 'enkel':
-          return ''
-        case 'middels':
-          return ''
-        case 'vanskelig':
-          return ''
-        default:
-          return ''
       }
     },
   },
@@ -41,50 +31,25 @@ export default {
 </script>
 
 <template>
-  <div class="container mx-auto" v-if="!isLoading">
-    <div class="flex flex-col items-center">
-      <h1 class="text-4xl font-bold my-5">Make Some</h1>
-      <a :href="`https://www.matprat.no${randomFoodDish.linkUrl}`" target="_blank">
-        <img
-          class="rounded h-80"
-          :src="`${randomFoodDish.imageUrl}-jumbotron/large`"
-          :alt="randomFoodDish.imageAlt"
-        />
-      </a>
-      <div class="mt-4 p-4 rounded border-4 border-gray-800">
-        <h1 class="text-4xl text-center w-100 font-bold">
-          {{ randomFoodDish.name }}
-        </h1>
-        <div>
-          <i :class="`pi pi-gauge ${getDifficultyIcon}`"></i>
-          <span class="mx-2">{{ randomFoodDish.difficulty }}</span>
-        </div>
-        <div>
-          <i class="pi pi-user"></i>
-          <span class="mx-1">{{ randomFoodDish.portions }}</span>
-          <!-- <div>
-            <i v-for="i in randomFoodDish.portions" class="pi pi-user mx-1"></i>
-          </div> -->
-        </div>
-        <div>
-          <i class="pi pi-clock"></i>
-          <span class="mx-2">{{ randomFoodDish.preparationTime }}</span>
-        </div>
-        <div>
-          <i v-for="i in randomFoodDish.rating" class="pi pi-star"></i>
-          <span class="mx-1"> {{ randomFoodDish.ratingPrecise }}</span>
-        </div>
-        <div>
-          <i class="pi pi-arrow-down mx-1"></i>
-          <span>Ingrendienser</span>
-        </div>
-      </div>
-      <button
-        @click="getRandomDinner"
-        class="text-white bg-gray-800 hover:bg-gray-700 hover:cursor-pointer font-medium rounded-lg mt-4 px-5 py-2.5"
-      >
-        Get Another Random Dinner
-      </button>
+  <i class="pi pi-spin pi-spinner" v-if="isLoading"></i>
+
+  <div class="flex flex-col items-center text-center h-screen" v-if="!isLoading">
+    <h1 class="text-4xl font-bold my-5">Make Some</h1>
+
+    <div class="flex flex-col items-center flex-grow max-h-[50vh] overflow-auto">
+      <FoodCard :food-dish="randomFoodDish" />
     </div>
+    <button
+      @click="getRandomDinner"
+      class="text-white bg-gray-800 font-medium rounded-lg mt-4 px-5 py-2.5 hover:bg-gray-700 hover:cursor-pointer"
+    >
+      Get Another Random Dinner
+    </button>
   </div>
 </template>
+
+<style>
+body {
+  background-color: oklch(0.938 0.127 124.321);
+}
+</style>
